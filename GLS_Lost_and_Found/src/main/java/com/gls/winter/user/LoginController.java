5a7d8a62dc3cf1266bd6cd37f769b8f3c9f13d41
JinwooLeeHGU <49421143+JinwooLeeHGU@users.noreply.github.com>
2021-02-22 19:51:16 +0900

    trying to solve google login

--- GLS_Lost_and_Found/src/main/java/com/gls/winter/user/LoginController.java
+++ GLS_Lost_and_Found/src/main/java/com/gls/winter/user/LoginController.java
@@ -53,8 +53,62 @@ public class LoginController {
 		System.out.println("googleCallback: Google login success");
 		model.addAttribute("list", boardService.getBoardList());
 		
+//		String username = request.getParameter("Given Name");
+////		String username = request.getParameter("username");
+//		
+////		session.setAttribute("userid", userid);
+//		session.setAttribute("username", username);
 		return "list"; 
 	}
+	/**
+	 * Authentication Code를 전달 받는 엔드포인트
+	 **/
+//	@GetMapping("google/auth")
+//	public String googleAuth(Model model, @RequestParam(value = "code") String authCode)
+//			throws JsonProcessingException {
+//		
+//		//HTTP Request를 위한 RestTemplate
+//		RestTemplate restTemplate = new RestTemplate();
+//
+//		//Google OAuth Access Token 요청을 위한 파라미터 세팅
+//		GoogleOAuthRequest googleOAuthRequestParam = GoogleOAuthRequest
+//				.builder()
+//				.clientId(clientId)
+//				.clientSecret(clientSecret)
+//				.code(authCode)
+//				.redirectUri("http://localhost:8080/login/google/auth")
+//				.grantType("authorization_code").build();
+//
+//		
+//		//JSON 파싱을 위한 기본값 세팅
+//		//요청시 파라미터는 스네이크 케이스로 세팅되므로 Object mapper에 미리 설정해준다.
+//		ObjectMapper mapper = new ObjectMapper();
+//		mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+//		mapper.setSerializationInclusion(Include.NON_NULL);
+//
+//		//AccessToken 발급 요청
+//		ResponseEntity<String> resultEntity = restTemplate.postForEntity(GOOGLE_TOKEN_BASE_URL, googleOAuthRequestParam, String.class);
+//
+//		//Token Request
+//		GoogleOAuthResponse result = mapper.readValue(resultEntity.getBody(), new TypeReference<GoogleOAuthResponse>() {
+//		});
+//
+//		//ID Token만 추출 (사용자의 정보는 jwt로 인코딩 되어있다)
+//		String jwtToken = result.getIdToken();
+//		String requestUrl = UriComponentsBuilder.fromHttpUrl("https://oauth2.googleapis.com/tokeninfo")
+//		.queryParam("id_token", jwtToken).encode().toUriString();
+//		
+//		String resultJson = restTemplate.getForObject(requestUrl, String.class);
+//		
+//		Map<String,String> userInfo = mapper.readValue(resultJson, new TypeReference<Map<String, String>>(){});
+//		model.addAllAttributes(userInfo);
+//		model.addAttribute("token", result.getAccessToken());
+//
+//
+//
+//		return "/google.html";
+//
+//	}
 
 	@RequestMapping(value = "/loginOk", method = RequestMethod.POST)
 	public String loginCheck(HttpSession session, UserVO vo, HttpServletRequest request) {
